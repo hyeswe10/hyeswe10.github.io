7ab6f79f81be30466a80e603f40df6ccc2a1a4d2
Jenny Moon <jmoon981107@gmail.com>
2025-06-05 21:28:31 +0900

    0605 애니메이션 처리까지 완료

--- paperlogy-sub/script/common.js
+++ paperlogy-sub/script/common.js
@@ -1,16 +1,32 @@
-gsap.registerPlugin(ScrollTrigger);
+
 
 //창 사이즈 별 js 파일 가져오기
-const checkDevice = ()=>{
+let currentScript = ''; // 전역에서 추적할 스크립트 종류
+
+const checkDevice = () => {
     const device = window.innerWidth <= 768 ? "mobile" : "pc";
+  // 이미 동일한 스크립트를 불러온 경우 재로드 방지
+    if (currentScript === device) {
+    return;
+    }
+    //이전 스크립트 제거
+    const $prev = document.querySelector(`script[data-script]`);
+    if( $prev ){
+        $prev.remove();
+        location.reload();
+    } 
     const script = document.createElement("script");
     script.src = `./script/${device}.js`;
+    script.setAttribute("data-script", device);
     document.body.appendChild(script);
-}
+    currentScript = device;
+};
 
-window.addEventListener("load",()=>{
+window.addEventListener("load", () => {
+    checkDevice();
+    gsap.registerPlugin(ScrollTrigger);
+});
+
+window.addEventListener("resize", () => {
     checkDevice();
 });
-// window.addEventListener("resize",()=>{
-//     checkDevice();
-// })
